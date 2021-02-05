b84be0bb50b5565d36017fea06e2fc5b191554ef
jadsonraphael1 <49865281+jadsonraphael1@users.noreply.github.com>
2021-02-04 21:41:28 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<DOCTYPE html>
+<html>
+  <head>
+    <title>Primeiro Repositório</title>
+  </head>
+  <body>
+    <h1>
+    <?php
+      echo "Esse é o meu primeiro código em repositório";
+    ?>
+      
+      <p><a href="novapagina.php">Link para nova página</a><p>
+    </h1>
+  </body>
+</html>
